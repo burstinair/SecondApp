47ba07e4bc8a7fd7e12b22c69cce04fa84e962b4
zhongkai.zhao <zhongkaizhao@192.168.0.109>
2014-08-03 14:44:53 +0800

    Initial Commit

---
+++ SecondApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SecondApp
+//
+//  Created by zhongkai.zhao on 14-8-3.
+//  Copyright (c) 2014年 zhongkai.zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SecondApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SecondApp
+//
+//  Created by zhongkai.zhao on 14-8-3.
+//  Copyright (c) 2014年 zhongkai.zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
